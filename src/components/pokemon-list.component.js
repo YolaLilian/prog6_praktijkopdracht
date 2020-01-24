b11438c4d717eac8d42f3a50f85c332732ccedb2
Lilian Kempees <0926313@hr.nl>
2020-01-24 19:58:46 +0100

    added flavourtext for components

---
+++ src/components/pokemon-list.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class ListPokemon extends Component {
+  render() {
+    return (
+      <div>
+        <p>React List Pokemon Component!</p>
+      </div>
+    );
+  }
+}

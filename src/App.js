68e3a9bf9f05b286fb388b377709d44f04a42aae
Lilian Kempees <0926313@hr.nl>
2020-01-24 20:04:31 +0100

    set-up navbar and browser-router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,70 @@
 import React from 'react';
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import CreatePokemon from "./components/create-pokemon.component";
+import EditPokemon from "./components/edit-pokemon.component";
+import ListPokemon from "./components/pokemon-list.component";
 
 function App() {
-  return (
+  return (<Router>
     <div className="App">
       <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+
+            <Navbar.Brand>
+              <Link to={"/create-pokemon"} className="nav-link">
+                Prog6 Praktijkopdracht 2
+              </Link>
+            </Navbar.Brand>
+
+            <Nav className="justify-content-end">
+              <Nav>
+                <Link to={"/create-pokemon"} className="nav-link">
+                  Create Pokemon
+                </Link>
+              </Nav>
+
+              {/* <Nav>
+                <Link to={"/edit-student/:id"} className="nav-link">
+                  Edit Student
+                </Link>
+              </Nav> */}
+
+              <Nav>
+                <Link to={"/pokemon-list"} className="nav-link">
+                  Pokemon List
+                </Link>
+              </Nav>
+            </Nav>
+
+          </Container>
+        </Navbar>
       </header>
+
+      <Container>
+        <Row>
+          <Col md={12}>
+            <div className="wrapper">
+              <Switch>
+                <Route exact path='/' component={CreatePokemon} />
+                <Route path="/create-student" component={CreatePokemon} />
+                <Route path="/edit-student/:id" component={EditPokemon} />
+                <Route path="/student-list" component={ListPokemon} />
+              </Switch>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
-  );
+  </Router>);
 }
 
 export default App;

ee8efdde09ea13437072a0669f0146213a7bca92
Lilian Kempees <0926313@hr.nl>
2020-01-24 19:53:32 +0100

    First commit

---
+++ src/components/create-pokemon.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class CreatePokemon extends Component {
+  render() {
+    return (
+      <div>
+        <p>React Create Pokemon Component!</p>
+      </div>
+    );
+  }
+}

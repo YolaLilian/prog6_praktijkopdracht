e227abf5d44684a17668acf48ff02461f68663aa
Lilian Kempees <0926313@hr.nl>
2020-01-29 23:21:24 +0100

    Added delete

--- src/components/PokemonTableRow.js
+++ src/components/PokemonTableRow.js
@@ -1,36 +1,52 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import axios from 'axios';
 import Button from 'react-bootstrap/Button';
+import { Redirect, withRouter } from 'react-router-dom';
 
-export default class PokemonTableRow extends Component {
+class PokemonTableRow extends Component {
     constructor(props) {
         super(props);
         this.deletePokemon = this.deletePokemon.bind(this);
+
+        this.state = {
+            redirect: false
+        }
     }
     
-    deletePokemon() {
-        axios.delete('http://localhost:4000/posts/delete-pokemon' + this.props.obj._id)
+    deletePokemon(e) {
+        e.preventDefault();
+        axios.delete('http://145.24.222.110:8000/posts/' + this.props.obj._id)
             .then((res) => {
                 console.log('Pokemon deleted successfully!!!')
             }).catch((error) => {
                 console.log(error)
             })
+            .then(() => this.setState({ redirect: true}));
     }
 
+    
     render() {
+        const { redirect } = this.state;
+
+        if (redirect) {
+            return <Redirect to="/" />
+        }
+        
         return (
             <tr>
                 <td>
                     {this.props.obj.baby}
                 </td>
                 <td>
-                    <Button variant="outline-success" size="sm" href={"edit-pokemon/" + this.props.obj._id}>Edit</Button>
+                    <Button variant="outline-info" size="sm" href={"edit-pokemon/" + this.props.obj._id}>Edit</Button>
                 </td>
                 <td>
-                    <Button onClick={this.deletePokemon} size="sm" variant="outline-danger">Delete this pokemon from existance</Button>           
+                    <Button onClick={this.deletePokemon} size="sm" variant="outline-danger">Delete</Button>           
                 </td>
             </tr>
         );
     }
 }
+
+export default withRouter(PokemonTableRow);

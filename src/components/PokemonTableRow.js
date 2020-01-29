f2bd4cd580848277e4cd95d34e7f3fa01d6c17d6
Lilian Kempees <0926313@hr.nl>
2020-01-29 23:56:58 +0100

    added modal for details pokemon

--- src/components/PokemonTableRow.js
+++ src/components/PokemonTableRow.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import Button from 'react-bootstrap/Button';
 import { Redirect, withRouter } from 'react-router-dom';
+import Modal from 'react-bootstrap/Modal';
 
 class PokemonTableRow extends Component {
     constructor(props) {
@@ -10,7 +11,8 @@ class PokemonTableRow extends Component {
         this.deletePokemon = this.deletePokemon.bind(this);
 
         this.state = {
-            redirect: false
+            redirect: false,
+            showModal: false
         }
     }
     
@@ -25,6 +27,8 @@ class PokemonTableRow extends Component {
             .then(() => this.setState({ redirect: true}));
     }
 
+    handleShow = () => this.setState({ showModal: true });
+    handleHide = () => this.setState({ showModal: false });
     
     render() {
         const { redirect } = this.state;
@@ -32,19 +36,42 @@ class PokemonTableRow extends Component {
         if (redirect) {
             return <Redirect to="/" />
         }
-        
+
         return (
-            <tr>
-                <td>
-                    {this.props.obj.baby}
-                </td>
-                <td>
-                    <Button variant="outline-info" size="sm" href={"edit-pokemon/" + this.props.obj._id}>Edit</Button>
-                </td>
-                <td>
-                    <Button onClick={this.deletePokemon} size="sm" variant="outline-danger">Delete</Button>           
-                </td>
-            </tr>
+            <>
+                <tr>
+                    <td>
+                        {/* {this.props.obj.baby} */}
+                        {/* <showModel /> */}
+                        <Button variant="outline-success" onClick={this.handleShow}>
+                            {this.props.obj.baby}
+                        </Button>
+                    </td>
+                    <td>
+                        <Button variant="outline-info" size="sm" href={"edit-pokemon/" + this.props.obj._id}>Edit</Button>
+                    </td>
+                    <td>
+                        <Button onClick={this.deletePokemon} size="sm" variant="outline-danger">Delete</Button>           
+                    </td>
+                </tr>
+
+                <Modal show={this.state.showModal} onHide={this.handleHide}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>
+                            Baby form: {this.props.obj.baby}
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        Teen form: {this.props.obj.teen}<br />
+                        Adult from: {this.props.obj.adult}
+                        </Modal.Body>
+                    <Modal.Footer>
+                    <Button variant="outline-dark" onClick={this.handleHide}>
+                        Close
+                    </Button>
+                    </Modal.Footer>
+                </Modal>
+            </>
         );
     }
 }
